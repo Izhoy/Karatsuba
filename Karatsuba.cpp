0deaf4a6e210fe2cba55b22d8d57b7a28fd9c04f
Izhoy <108609622+Izhoy@users.noreply.github.com>
2022-08-27 13:22:23 +0300

    Update Karatsuba.cpp

--- Karatsuba.cpp
+++ Karatsuba.cpp
@@ -3,21 +3,19 @@
 
 using namespace std;
 
-string operator*(const string,const string); // перегрузка оператора умножения
-string operator+(const string,const string); // перегрузка оператора сложения
-string operator-(const string,const string); // перегрузка оператора вычитания
-bool operator>(const string, const string); // перегрузка операторов сравнения
+string operator*(const string,const string); // overloading the multiplication operator
+string operator+(const string,const string); // overloading the addition operator
+string operator-(const string,const string); // overloading the substraction operator
+bool operator>(const string, const string); // overloading the comprasion operators
 bool operator<(const string, const string);
 bool operator==(const string, const string);
 
-string multiplication_by_10(string&, size_t); // функция умножения числа на 10 в степени
+string multiplication_by_10(string&, size_t); // multiplication by 10 to the power
 
 
 int main() {
 
-	
-
-	/*string a = "3141592653589793238462643383279502884197169399375105820974944592";
+	string a = "3141592653589793238462643383279502884197169399375105820974944592";
 	string b = "2718281828459045235360287471352662497757247093699959574966967627";
 
 	cout << "a:\t" << a << endl;
@@ -27,31 +25,31 @@ int main() {
 	
 	cout << "a+b: \t" << a + b << endl;
 
-	cout << "a*b: \t" << a * b << endl;*/
+	cout << "a*b: \t" << a * b << endl;
 
 	return EXIT_SUCCESS;
 }
 
-// Умножение двух чисел методом Карацубы
+// overloading the multiplication operator
 string operator*(const string Str1,const string Str2) {
-	// Принятие решения в зависимости от знака
+	// Making a decision depending on the sign
 	if (Str1.at(0) == '-' && Str2.at(0) == '-') return Str1.substr(1) * Str2.substr(1);
 	else if (Str1.at(0) == '-' && Str2.at(0) != '-') return (Str1.substr(1) * Str2).insert(0, 1, '-');
 	else if (Str1.at(0) != '-' && Str2.at(0) == '-') return (Str1 * Str2.substr(1)).insert(0, 1, '-');
 
 	string Production;
 
-	// Базовый случай
+	// Base case
 	if (Str1.size() == 1 && Str2.size() == 1) {
 		Production = to_string(stoi(Str1) * stoi(Str2));
 		return Production;
 	}
 
-	// Рекурсивный случай
+	// Recursion case
 	else {
 		string a, b, c, d, ac, bd, bc, ad;
 		
-		// Разбиваем первый множитель на 2 части (a и b)
+		// Split first number by 2 parts (a and b)
 		if (Str1.size() == 1) {
 			a = Str1;
 			b = "0";
@@ -63,7 +61,7 @@ string operator*(const string Str1,const string Str2) {
 			}
 		}
 
-		// Разбиваем второй множитель на 2 части (c и d)
+		// Split second number by 2 parts (c and d)
 		if (Str2.size() == 1) {
 			c = Str2;
 			d = "0";
@@ -74,13 +72,13 @@ string operator*(const string Str1,const string Str2) {
 				d.push_back(Str2.at(Str2.size() / 2 + i));
 			}
 		}
-		// Рекурсивно вычисляем
+		// Recursively calculate
 		bc = b * c;
 		ac = a * c;
 		bd = b * d;
 		ad = a * d;
 
-		// Финальная формула
+		// The final formula
 		Production = multiplication_by_10(ac, Str1.size()/2 + Str2.size()/2) + 
 			multiplication_by_10(ad, Str1.size() / 2) + multiplication_by_10(bc, Str2.size()/2) + bd;
 	}
@@ -88,9 +86,9 @@ string operator*(const string Str1,const string Str2) {
 	return Production;
 }
 
-// Сложение двух чисел столбиком
+// overloading the addition operator
 string operator+(const string Str1,const string Str2){
-	// Принятие решения в зависимости от знаков чисел
+	// Making a decision depending on the sign
 	if (Str1.at(0) == '-' && Str2.at(0) != '-')	return Str2 - Str1.substr(1);
 	else if (Str1.at(0) != '-' && Str2.at(0) == '-') return Str1 - Str2.substr(1);
 	else if (Str1.at(0) == '-' && Str2.at(0) == '-') return (Str1.substr(1) + Str2.substr(1)).insert(0, 1, '-');
@@ -99,9 +97,9 @@ string operator+(const string Str1,const string Str2){
 	int addition = 0;
 
 	for (int i = (int)Str1.size() - 1, j = (int)Str2.size() - 1; i >= 0 || j >= 0; --i, --j) {
-		char number1 = i < 0 ? 0 : Str1.at(i) - '0'; // Дополняем числа до одинаковой длины дополнительными нулями
-		char number2 = j < 0 ? 0 : Str2.at(j) - '0'; // И переводим из символов в числа путём вычитания '0'
-		char result = number1 + number2 + addition; // Начинаем сложение с меньших разрядов
+		char number1 = i < 0 ? 0 : Str1.at(i) - '0'; // We complement the numbers to the same length with additional zeros
+		char number2 = j < 0 ? 0 : Str2.at(j) - '0'; // And we translate from characters to numbers by subtracting '0'
+		char result = number1 + number2 + addition; // We begin the addition with smaller digits
 		if (result > 9) {
 			result = result - 10;
 			addition = 1;
@@ -114,7 +112,7 @@ string operator+(const string Str1,const string Str2){
 
 	if (addition == 1) Sum.push_back('1');
 
-	// Меняем разряды числа между собой
+	// We change the digits of the number among themselves
 	for (size_t i = 0; i < Sum.size() / 2; ++i) {
 		swap(Sum.at(i), Sum.at(Sum.size() - i - 1));
 	}
@@ -122,22 +120,22 @@ string operator+(const string Str1,const string Str2){
 	return Sum;
 }
 
-// Вычитание двух чисел столбиком
+// overloading the substraction operator
 string operator-(const string Str1, const string Str2){
-	// Принятие решения в зависимости от знаков чисел
+	// Making a decision depending on the sign
 	if (Str1.at(0) == '-' && Str2.at(0) != '-')	return (Str1.substr(1) + Str2).insert(0, 1, '-');
 	else if (Str1.at(0) != '-' && Str2.at(0) == '-') return Str1 + Str2.substr(1);
 	else if (Str1.at(0) == '-' && Str2.at(0) == '-') return Str2.substr(1) - Str1.substr(1);
 
-	// В случае, когда второе число больше первого менеям числа местами и к резултату добавляем '-'
+	// In the case when the second number is greater than the first, we swap the numbers and add '-' to the result
 	if (Str2 > Str1) return (Str2 - Str1).insert(0, 1, '-');
 
 	string Sub;
 	int substraction = 0;
 
 	for (int i = (int)Str1.size() - 1, j = (int)Str2.size() - 1; i >= 0 || j >= 0; --i, --j) {
-		char number1 = i < 0 ? 0 : Str1.at(i) - '0'; // Дополняем числа до одинаковой длины дополнительными нулями
-		char number2 = j < 0 ? 0 : Str2.at(j) - '0'; // И переводим из символов в числа путём вычитания '0'
+		char number1 = i < 0 ? 0 : Str1.at(i) - '0'; // We complement the numbers to the same length with additional zeros
+		char number2 = j < 0 ? 0 : Str2.at(j) - '0'; // And we translate from characters to numbers by subtracting '0'
 		char result = number1 - number2 - substraction;
 
 		if (result >= 0) {
@@ -150,13 +148,13 @@ string operator-(const string Str1, const string Str2){
 		Sub.push_back(result + '0');
 	}
 
-	// Удаляем нули из начала числа
+	// Removing zeros from the beginning of the number
 	for (size_t i = Sub.size() - 1; i > 0; --i) {
 		if (Sub.at(i) == '0') Sub.pop_back();
 		else break;
 	}
 
-	// Меняем разряды между собой
+	// We change the digits of the number among themselves
 	for (size_t i = 0; i < Sub.size() / 2; ++i) {
 		swap(Sub.at(i), Sub.at(Sub.size() - i - 1));
 	}
@@ -164,14 +162,14 @@ string operator-(const string Str1, const string Str2){
 	return Sub;
 }
 
-// Перегрузка оператора сравнения
+// overloading the comprasion operator
 bool operator>(const string Str1, const string Str2){
-	// Принятие решения в зависимости от знака числа
+	// Making a decision depending on the sign
 	if (Str1.at(0) == '-' && Str2.at(0) != '-')	return false;
 	else if (Str1.at(0) != '-' && Str2.at(0) == '-') return true;
 	else if (Str1.at(0) == '-' && Str2.at(0) == '-') return Str1.substr(1) > Str2.substr(1);
 
-	// Принятие решения в зависимости от количества разрядов чисел
+	// Making a decision depending on the number of digits of numbers
 	if (Str1.size() > Str2.size()) return true;
 	else if (Str1.size() == Str2.size()) {
 		for (size_t i = 0; i < Str1.size(); ++i) {
@@ -183,14 +181,14 @@ bool operator>(const string Str1, const string Str2){
 	else return false;
 }
 
-// Перегрузка оператора сравнения
+// overloading the comprasion operator
 bool operator<(const string Str1, const string Str2){
-	// Принятие решения в зависимости от знака числа
+	// Making a decision depending on the sign
 	if (Str1.at(0) == '-' && Str2.at(0) != '-')	return true;
 	else if (Str1.at(0) != '-' && Str2.at(0) == '-') return false;
 	else if (Str1.at(0) == '-' && Str2.at(0) == '-') return Str1.substr(1) < Str2.substr(1);
 
-	// Принятие решения в зависимости от количества разрядов чисел
+	// Making a decision depending on the number of digits of numbers
 	if (Str1.size() < Str2.size()) return true;
 	else if (Str1.size() == Str2.size()) {
 		for (size_t i = 0; i < Str1.size(); ++i) {
@@ -202,7 +200,7 @@ bool operator<(const string Str1, const string Str2){
 	else return false;
 }
 
-// Перегрузка оператора равенства
+// overloading the equality operator
 bool operator==(const string Str1, const string Str2){
 
 	if (Str1.size() == Str2.size()) {
@@ -216,9 +214,9 @@ bool operator==(const string Str1, const string Str2){
 	}
 }
 
-// Умножение числа на 10 в степени power
+// Multiplying a number by 10 to the power of "power"
 string multiplication_by_10(string &number, size_t power){
-	// Добавляем к числу количество нулей равное степени 10
+	// Add to the number the number of zeros equal to the power of 10
 	if (number.at(0) != '0') {
 		for (size_t i = 0; i < power; ++i) {
 			number.push_back('0');
